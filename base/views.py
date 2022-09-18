e0439f25614021b2defb64f667af203b09a7c8c8
jhanaveenk <jhanaveenk@u.github.com>
2022-09-18 05:37:18 +0530

    commit on 17 Sept

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room , Topic
 from django.contrib.auth.models import User
 from .forms import RoomForm
@@ -12,15 +13,15 @@ from .forms import RoomForm
 
 
 def loginPage(request):
+    page = 'login'
     
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
-##flash Messeages is not working
         try:
             user = User.objects.get(username = username)
         except:
@@ -34,12 +35,28 @@ def loginPage(request):
         else:
             messages.error(request, 'Username Or password does not exists')
 
-    context = {}
+    context = {'page' : page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during Regestation')
+            
+    return render (request, 'base/login_register.html', {'form': form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
