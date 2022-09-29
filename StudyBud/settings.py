5df270b262d5ef461211f159bd8121f2fa3b0777
jhanaveenk <jhanaveenk@u.github.com>
2022-09-29 15:13:01 +0530

    N Commit

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 STATIC_URL = '/static/'
 
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT =BASE_DIR / 'static/images'
+MEDIA_ROOT =BASE_DIR/'static/images'
 
 
 
