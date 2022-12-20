1a2a1fd3fd8493463c82793ee75aa88c9bbbf1ee
jhanaveenk <jhanaveen255@gmail.com>
2022-12-21 03:19:32 +0530

    Deployment -4

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://netspire.up.railway.app', 'http://127.0.0.1']
 
 ROOT_URLCONF = 'StudyBud.urls'
 
